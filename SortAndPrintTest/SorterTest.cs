5fc86c6d97c32be8944e522f8fed8d8e341ef598
Light Handle <kunalthorat@gmail.com>
2016-10-23 15:40:46 -0400

    Additional Unit Tests for Sorter

--- SortAndPrintTest/SorterTest.cs
+++ SortAndPrintTest/SorterTest.cs
@@ -17,11 +17,62 @@ namespace SortAndPrintTest
             ISorter sorter = new Sorter();
             sorter.SortByFileNameAlphabetically(emptyContentList);
         }
-    }
 
-   /* [TestClass]
-    public void  ShouldReturnSortedListByFileContent()
-    {
+        [TestMethod]
+        public void ShouldReturnListSortedByFileContent()
+        {
+            List<Content> unsortedList = new List<Content>
+            {
+                new Content
+                {
+                    FolderName = "SortedMock",
+                    FileName = "cd46fc09-73ee-4c6b-8c02-ca7ad48a359b.txt",
+                    FileContent = 322
+                },
+                new Content
+                {
+                    FolderName = "SortedMock",
+                    FileName = "34e4a0ad-e9d4-45d4-8967-e2e46330e096.txt",
+                    FileContent = 227
+                }
+            };
 
-    }*/
+            List<Content> expectedSortedList = new List<Content>
+            {
+                new Content
+                {
+                    FolderName = "SortedMock",
+                    FileName = "34e4a0ad-e9d4-45d4-8967-e2e46330e096.txt",
+                    FileContent = 227
+                },
+                new Content
+                {
+                    FolderName = "SortedMock",
+                    FileName = "cd46fc09-73ee-4c6b-8c02-ca7ad48a359b.txt",
+                    FileContent = 322
+                }
+            };
+
+            ISorter sorter = new Sorter();
+            List<Content> actualSortedList = sorter.SortByFileNameAlphabetically(unsortedList);
+
+            CollectionAssert.AreEqual(expectedSortedList, actualSortedList, new ContentListComparer());
+        }
+
+        private class ContentListComparer : Comparer<Content>
+        {
+            public override int Compare(Content x, Content y)
+            {
+                int comparison = string.Compare(x.FolderName, y.FolderName);
+                if (comparison != 0)
+                    return comparison;
+
+                comparison = string.Compare(x.FileName, y.FileName);
+                if (comparison != 0)
+                    return comparison;
+
+                return string.Compare(Convert.ToString(x.FileContent), Convert.ToString(y.FileContent));
+            }
+        }
+    }   
 }
